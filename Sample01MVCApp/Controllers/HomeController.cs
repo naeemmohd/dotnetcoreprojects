a9de4b19a23938b3ec70c5ab8e4686ff6bf50c16
Mohd Naeem <naeem.mohd@hotmail.com>
2023-08-27 20:59:55 -0500

    commits for ActionResult Types

--- Sample01MVCApp/Controllers/HomeController.cs
+++ Sample01MVCApp/Controllers/HomeController.cs
@@ -1,56 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Sample01MVCApp.Models;
 
 namespace Sample01MVCApp.Controllers
 {
-    [Route("[controller]/[action]")]
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	[Route("[controller]/[action]")]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        [Route("/")]
-        public IActionResult Index()
-        {
-            TempData["AppName"]= "Training App";
-            TempData.Keep();
-            return View();
-        }
+		[Route("/")]
+		public IActionResult Index()
+		{
+			TempData["AppName"]= "Training App";
+			TempData.Keep();
+			return View();
+		}
 
-        [Route("/{Id?}")]
-        public IActionResult Index(int Id)
-        {
-            TempData["AppName"]= "Training App " + Id;
-            TempData.Keep();
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		[Route("/{Id?}")]
+		public IActionResult Index(int Id)
+		{
+			TempData["AppName"]= "Training App " + Id;
+			TempData.Keep();
+			return View();
+		}
+		
+		[Route("Home/Privacy")]
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
-        public JsonResult GetStudentDetails(int Id)
-        {
-            StudentRepository repository = new StudentRepository();
-            Student student = repository.GetStudentById(Id);
+		public JsonResult GetStudentDetails(int Id)
+		{
+			StudentRepository repository = new StudentRepository();
+			Student student = repository.GetStudentById(Id);
 
-            return Json(student);
+			return Json(student);
+		}
+		public FileResult GetFile()
+		{
+			//Get the File Path
+			string filePath = Directory.GetCurrentDirectory() + "\\wwwroot\\PDFFiles\\" + "HOAForm.pdf";
+		   
+			//Convert to Byte Array
+			byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+			var contentDisposition = new ContentDisposition
+			{
+				FileName = "HOAForm.pdf",
+				Inline = true // Set to true if you want to display the file in the browser
+			};
+			Response.Headers.Add("Content-Disposition", contentDisposition.ToString());
+			//Return the Byte Array
+			return File(fileBytes, "application/pdf");
+		}
+		public RedirectResult GoToGoogle()
+		{
+			return Redirect("https://www.google.com");
+		}		
+		public RedirectToActionResult ToStudentDetailsAction()
+		{
+			return RedirectToAction("GetStudentDetails");
+		}
+		public RedirectToRouteResult ToProvicyRoute()
+		{
+			return RedirectToRoute("Home/Privacy");
+		}
+		public IActionResult NotFoundExample()
+		{
+			// Return a 404 Not Found response
+			return new StatusCodeResult(404);
+		}
+		public IActionResult CustomStatusCodeExample()
+		{
+			// Return a custom status code
+			return new StatusCodeResult(403); 
+		}
+		public IActionResult BadRequestExample()
+        {
+            // Return a BadRequestResult
+            return new BadRequestResult();
         }
-    }
+        public IActionResult OKResultExample()
+        {
+            // Return a OK result
+            return new OkResult(); 
+        }
+	}
 }
--- Sample01MVCApp/Controllers/StudentController.cs
+++ Sample01MVCApp/Controllers/StudentController.cs
@@ -1,140 +1,146 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sample01MVCApp.Models;
 
 namespace Sample01MVCApp.Controllers
 {
-    public class StudentController : Controller
-    {
-        private readonly IStudentRepository _studentRepository;
-        public StudentController(IStudentRepository studentRepository)
-        {
-            _studentRepository=studentRepository;
-        }
+	public class StudentController : Controller
+	{
+		private readonly IStudentRepository _studentRepository;
+		public StudentController(IStudentRepository studentRepository)
+		{
+			_studentRepository=studentRepository;
+		}
 
-        [Route("Student/ById")]
-        public IActionResult Index(int Id=101)
-        {
-            Student student = _studentRepository.GetStudentById(Id);
-            return View(student);
-        }
+		[Route("Student/ById")]
+		public IActionResult Index(int Id=101)
+		{
+			Student student = _studentRepository.GetStudentById(Id);
+			return View(student);
+		}
 
-        [Route("Student/ByJSONId/{Id=101}")]
-        public JsonResult GetStudentById(int Id)
-        {
-            return Json(_studentRepository.GetStudentById(Id));
-        }
+		[Route("Student/ByJSONId/{Id=101}")]
+		public JsonResult GetStudentById(int Id)
+		{
+			return Json(_studentRepository.GetStudentById(Id));
+		}
 
-        [Route("Student/All")]
-        public JsonResult GetAllStudents()
-        {
-            return Json(_studentRepository.GetAllStudents());
-        }
+		[Route("Student/All")]
+		public JsonResult GetAllStudents()
+		{
+			var students = _studentRepository.GetAllStudents();
+			var options = new JsonSerializerOptions()
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			};
+			return Json(students, options);
+		}
 
-        [Route("Student/ByName/name=naeem")]
-        public string GetStudentsByName(string name)
-        {
-            return $"Return All Students with Name : {name}";
-        }   
-        public ViewResult DetailsViewData()
-        {
-            ViewData["Title"]= "Student Details Page";
-            ViewData["Header"]= "Student Details using ViewData as Dictionary";
+		[Route("Student/ByName/name=naeem")]
+		public string GetStudentsByName(string name)
+		{
+			return $"Return All Students with Name : {name}";
+		}   
+		public ViewResult DetailsViewData()
+		{
+			ViewData["Title"]= "Student Details Page";
+			ViewData["Header"]= "Student Details using ViewData as Dictionary";
 
-            var student =new  Student(){
-                StudentId = 101,
-                Name = "James",
-                Branch = "CSE",
-                Section = "A",
-                Gender = "Male"
-            };
-            ViewData["Student"]= student;
+			var student =new  Student(){
+				StudentId = 101,
+				Name = "James",
+				Branch = "CSE",
+				Section = "A",
+				Gender = "Male"
+			};
+			ViewData["Student"]= student;
 
-            return View();
+			return View();
 
-        }
-        public ViewResult DetailsViewBag()
-        {
-            ViewBag.Title = "Student Details Page";
-            ViewBag.Header = "Student Details using ViewBag as dynamic property/variable";
-            Student student = new Student()
-            {
-                StudentId = 101,
-                Name = "James",
-                Branch = "CSE",
-                Section = "A",
-                Gender = "Male"
-            };
-            ViewBag.Student = student;
-            return View();
+		}
+		public ViewResult DetailsViewBag()
+		{
+			ViewBag.Title = "Student Details Page";
+			ViewBag.Header = "Student Details using ViewBag as dynamic property/variable";
+			Student student = new Student()
+			{
+				StudentId = 101,
+				Name = "James",
+				Branch = "CSE",
+				Section = "A",
+				Gender = "Male"
+			};
+			ViewBag.Student = student;
+			return View();
 
-        }
+		}
 
-        public ViewResult Details()
-        {
-            ViewBag.Title = "Student Details Page";
-            ViewBag.Header = "Student Details using Strongly typed model";
-            Student student = new Student()
-            {
-                StudentId = 101,
-                Name = "James",
-                Branch = "CSE",
-                Section = "A",
-                Gender = "Male"
-            };
-            
-            return View(student);
+		public ViewResult Details()
+		{
+			ViewBag.Title = "Student Details Page";
+			ViewBag.Header = "Student Details using Strongly typed model";
+			Student student = new Student()
+			{
+				StudentId = 101,
+				Name = "James",
+				Branch = "CSE",
+				Section = "A",
+				Gender = "Male"
+			};
+			
+			return View(student);
 
-        }
-        public ViewResult DetailsViewModel()
-        {
-            Student student = new Student()
-            {
-                StudentId = 101,
-                Name = "Dillip",
-                Branch = "CSE",
-                Section = "A",
-                Gender = "Male"
-            };
-            //Student Address
-            Address address = new Address()
-            {
-                StudentId = 101,
-                City = "Mumbai",
-                State = "Maharashtra",
-                Country = "India",
-                Pin = "400097"
-            };
-            
-            StudentDetailsViewModel studentDetailsViewModel = new StudentDetailsViewModel(){
-                Student = student,
-                Address = address,
-                Header="Student Details",
-                Title = "Student Details Page"
+		}
+		public ViewResult DetailsViewModel()
+		{
+			Student student = new Student()
+			{
+				StudentId = 101,
+				Name = "Dillip",
+				Branch = "CSE",
+				Section = "A",
+				Gender = "Male"
+			};
+			//Student Address
+			Address address = new Address()
+			{
+				StudentId = 101,
+				City = "Mumbai",
+				State = "Maharashtra",
+				Country = "India",
+				Pin = "400097"
+			};
+			
+			StudentDetailsViewModel studentDetailsViewModel = new StudentDetailsViewModel(){
+				Student = student,
+				Address = address,
+				Header="Student Details",
+				Title = "Student Details Page"
 
-            };
-            return View(studentDetailsViewModel);
+			};
+			return View(studentDetailsViewModel);
 
-        }
-        //URL Pattern: /Student/1/Courses
-        [Route("Student/{studentID=101}/Courses")] 
-        public JsonResult GetStudentCourses(int studentID)
-        {
-            //Real-Time you will get the courses from database, here we have hardcoded the data
-            List<string> CourseList = new List<string>();
-            if (studentID == 101)
-                CourseList = new List<string>() { "ASP.NET Core", "C#.NET", "SQL Server" };
-            else if (studentID == 102)
-                CourseList = new List<string>() { "ASP.NET Core MVC", "C#.NET", "ADO.NET Core" };
-            else if (studentID == 103)
-                CourseList = new List<string>() { "ASP.NET Core WEB API", "C#.NET", "Entity Framework Core" };
-            else
-                CourseList = new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
-            
-            return Json(CourseList);
-        }
-    }
+		}
+		//URL Pattern: /Student/1/Courses
+		[Route("Student/{studentID=101}/Courses")] 
+		public JsonResult GetStudentCourses(int studentID)
+		{
+			//Real-Time you will get the courses from database, here we have hardcoded the data
+			List<string> CourseList = new List<string>();
+			if (studentID == 101)
+				CourseList = new List<string>() { "ASP.NET Core", "C#.NET", "SQL Server" };
+			else if (studentID == 102)
+				CourseList = new List<string>() { "ASP.NET Core MVC", "C#.NET", "ADO.NET Core" };
+			else if (studentID == 103)
+				CourseList = new List<string>() { "ASP.NET Core WEB API", "C#.NET", "Entity Framework Core" };
+			else
+				CourseList = new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+			
+			return Json(CourseList);
+		}
+	}
 }
