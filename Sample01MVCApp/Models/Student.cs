c94b08e17ae4895ea3992753c2466c5569392dc4
Mohd Naeem <naeem.mohd@hotmail.com>
2023-09-04 20:33:02 -0500

    updates for MVC Core App

---
+++ Sample01MVCApp/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace Sample01MVCApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public string? Branch { get; set; }
+        public string? Section { get; set; }
+        public string? Gender { get; set; }
+    }
+}
